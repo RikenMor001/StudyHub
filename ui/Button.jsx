ee45d7571bc70f2e5971b73a0995d1986e334a9b
Riken Patel <149461465+RikenMor001@users.noreply.github.com>
2025-04-28 17:56:58 -0400

    Update Button.jsx

--- ui/Button.jsx
+++ ui/Button.jsx
@@ -1,9 +1,15 @@
+import * as React from "React"
 
+const Button = React.forwardRef(({className, vairant, size, asChild = false, ...props}, ref) => {
+    const Comp = asChild ? Slot : "button"
+    return (|
+        <Comp
+        className = {cn}
+        ref = {ref}
+        {...props}
+        />
+    )
+})
 
-export function Button(){
-    return <div> 
-        <button className="">
-            Toggle Key
-        </button>
-    </div>
-}
+Button.deisplayName = "Button"
+export { Button }
