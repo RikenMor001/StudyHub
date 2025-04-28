e4a4631417d4eafc8a89023a9b1ab23ac915cb3d
Riken Patel <149461465+RikenMor001@users.noreply.github.com>
2025-04-28 19:24:13 -0400

    Update Button.jsx

--- ui/Button.jsx
+++ ui/Button.jsx
@@ -1,11 +1,13 @@
-import * as React from " react "
+
 import { cn } from "../lib/utils"
 import { cva } from "class-variance-authority"
+import { Slot } from "@radix-ui/react-slot"
+import * as React from "react"
 
 const buttonVariants = cva({
     variants: {
         variant: {
-            
+
         },
         size: {
 
@@ -13,15 +15,14 @@ const buttonVariants = cva({
     }
 })
 
-const Button = React.forwardRef(({className, variant, size, asChild = false, ...props}), ref => {
-    const Comp = asChild ? Slot : "button"
+export const Button = React.forwardRef(({className, variant, size, asChild = false, ...props}, ref) => {
+    const Comp = asChild ? Slot : " button ";
     return (
         <Comp
-        className = {cn(buttonVariants(varirant, size, className))} 
+        className = {cn(buttonVariants({className, variant, size}))}
         ref = { ref }
         {...props}
-        />
+        /> 
     )
-}) 
-Button.displayName = "Button"
-export { Button , buttonVariants }
+})
+Button.displayName = "button"
