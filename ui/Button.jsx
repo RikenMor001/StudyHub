6c49f03e86fe5ba8da020913d91fa0276b2a16e3
Riken Patel <149461465+RikenMor001@users.noreply.github.com>
2025-04-29 16:20:39 -0400

    Update Button.jsx

--- ui/Button.jsx
+++ ui/Button.jsx
@@ -1,28 +1,33 @@
 
+import { Slot } from "@radix-ui/react-slot"  // slot helps you pass children within, without harming/disrupting tailwindcss dynamicaly
+import * as React from "react"
 import { cn } from "../lib/utils"
 import { cva } from "class-variance-authority"
-import { Slot } from "@radix-ui/react-slot"
-import * as React from "react"
 
-const buttonVariants = cva({
+export const buttonVariants = cva({
+    // variants are yet to be added, this is jsut to make it work temporarily 
     variants: {
         variant: {
-
+            default: "text-slate-500"
         },
         size: {
-
+            default: "h-5 w-5"
         }
     }
 })
 
-export const Button = React.forwardRef(({className, variant, size, asChild = false, ...props}, ref) => {
-    const Comp = asChild ? Slot : " button ";
+export const Button = React.forwardRef(({className, size, variant, asChild = false, ...props}, ref) => {
+    const Comp = asChild ? Slot : "button"
     return (
         <Comp
-        className = {cn(buttonVariants({className, variant, size}))}
-        ref = { ref }
+        className = {cn(buttonVariants({variant, size, className}))}
+        ref = {ref}
         {...props}
-        /> 
+        />
     )
 })
-Button.displayName = "button"
+
+Button.displayName = "Button"
+
+
+
