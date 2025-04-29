746d1d02a41652f0cfbe7ebfc44b23788e1d534a
Riken Patel <149461465+RikenMor001@users.noreply.github.com>
2025-04-29 16:20:11 -0400

    Update Navbar.jsx

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -6,44 +6,44 @@ import { useTheme } from "./theme-provider"
 export function NavBar(){
 const { theme, setTheme } = useTheme(); 
 
-    return <header className="font-semibold text-lg sticky top-0 z-50 w-full border-b bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
+    return <header className="font-semibold text-md sticky top-0 z-50 w-full border-b bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
         <div className="container mx-auto px-5">
-            <div className="flex h-14 items-center justify-between">
+            <div className="flex h-11 items-center justify-between">
                 <Link
                 to="/"
-                className="flex items-center gap-2 text-lg font-semibold transition-colors hover:text-primary"
+                className="flex items-center gap-2 text-md font-semibold transition-colors hover:text-primary"
                 > 
-                    <Book className="h-6 w-6"/>  
+                    <Book className="h-5 w-5"/>  
                     <span>StudyHub</span>
                 </Link>
 
                 <nav className="flex items-center gap-4">
                 <Link
                     to="/habit-builder"
-                    className="flex items-center gap-2 text-lg font-semibold transition-colors hover:text-primary"
+                    className="flex items-center gap-2 text-md font-semibold transition-colors hover:text-primary"
                     >
                     <Clock
-                    className="h-6 w-6"
+                    className="h-5 w-5"
                     />
                     <span>Study Tracker</span>
                 </Link>
 
                 <Link
                     to="/habit-builder"
-                    className="flex items-center gap-2 text-lg font-semibold transition-colors hover:text-primary"
+                    className="flex items-center gap-2 text-md font-semibold transition-colors hover:text-primary"
                     >
                     <Dumbbell
-                    className="h-6 w-6"
+                    className="h-5 w-5"
                     />
                     <span>Habit Builder</span>
                 </Link>
 
                 <Link
                     to="/habit-builder"
-                    className="flex items-center gap-2 text-lg font-semibold transition-colors hover:text-primary"
+                    className="flex items-center gap-2 text-md font-semibold transition-colors hover:text-primary"
                     >
                     <BookOpen
-                    className="h-6 w-6"
+                    className="h-5 w-5"
                     />
                     <span>Resources</span>
                 </Link>
