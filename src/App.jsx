fc8dc22671c2a61602d5af3581ebb1f1d0d694ae
Riken Patel <149461465+RikenMor001@users.noreply.github.com>
2025-04-29 17:39:06 -0400

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,23 @@
 // im using CONTEXT SWITCHING to avoid prop drilling so my main application is wrapped inside a <ThemeProvider><ThemeProvider/> 
 
+import { BrowserRouter, Route, Router, Routes } from 'react-router-dom'
 import './App.css'
 import { NavBar } from './components/navbar'
 import { ThemeProvider } from './components/theme-provider'
+import LandingPage from './pages/LandingPage'
 
 function App() {
   return <div>
-    <ThemeProvider>
-      <NavBar/>
-    </ThemeProvider>
+    <BrowserRouter>
+      <ThemeProvider>
+        <div>
+          <NavBar/>
+          <Routes>
+            <Route path='/' element = {<LandingPage/>}/>
+          </Routes>
+        </div>
+        </ThemeProvider>
+      </BrowserRouter>
   </div>
 }
 
