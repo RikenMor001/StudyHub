80cad5341a44185aa5b19904b99feadbbd9559f3
Riken Patel <149461465+RikenMor001@users.noreply.github.com>
2025-04-29 17:38:38 -0400

    Add files via upload

---
+++ pages/LandingPage.jsx
@@ -0,0 +1,8 @@
+
+export default function LandingPage(){
+    return (
+        <div className="text-center items-cennter felx justify-center">
+            Hello from landing page
+        </div>
+    )
+}
