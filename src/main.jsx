843feac18761f284180aa6a86b4ea3e390771d2f
Riken Patel <149461465+RikenMor001@users.noreply.github.com>
2025-04-29 17:42:19 -0400

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,12 @@
+// When using components from react-router-dom, in the main.jsx file the App tag has to be wrapped inside a <BrowserRoter><BrowserRoter/>
+
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import { BrowserRouter } from 'react-router-dom'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter>
       <App />
-    </BrowserRouter>
   </StrictMode>,
 )
