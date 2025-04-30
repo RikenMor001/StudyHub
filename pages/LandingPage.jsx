6222fb2ea97f04ebd09643cfbab5ab7b4d0ec730
Riken Patel <149461465+RikenMor001@users.noreply.github.com>
2025-04-30 17:03:50 -0400

    Update LandingPage.jsx

--- pages/LandingPage.jsx
+++ pages/LandingPage.jsx
@@ -1,4 +1,5 @@
 import { Button } from "../ui/Button";
+import { motion } from "framer-motion"
 
 export default function LandingPage(){
     return (
@@ -7,12 +8,13 @@ export default function LandingPage(){
                 <section className="relative py-20 px-5"> 
                     <div className="absolute inset-0 bg-gradient-to-b from-primary/5 to-transparent"></div>
                     <div className="container mx-auto max-w-6xl relative">
+                        <motion.div>
                         <h1 className="text-5xl md:text-7xl font-bold -tracking-tight mb-6">
                             <span className="bg-gradient-to-r from-primary to-purple-800 bg-clip-text text-transparent">
                                 Transform Your Study Journey                                
                             </span>
                         </h1>
-                        <p className="text-xl text-muted-foreground max-w-3xl mx-auto mb-8">Elevate your academic peformance with our intelligent study tracking habit-biling tools, and AI-powered insights</p>
+                        <p className="text-xl text-slate-500 font-semibold max-w-3xl mx-auto mb-8">Elevate your academic peformance with our intelligent study tracking habit-biling tools, and AI-powered insights</p>
                         <div className="flex flex-wrap justify-center gap-8">
 
                             <Button
@@ -30,6 +32,7 @@ export default function LandingPage(){
                                 Learn More
                             </Button>
                         </div>
+                        </motion.div>
                     </div>                                            
                 </section>
             </main>
