66fe88166cf27cf64c9eca9b9c5103aa74dfe5c3
Riken Patel <149461465+RikenMor001@users.noreply.github.com>
2025-05-01 16:35:26 -0400

    Update axios.js

--- api/axios.js
+++ api/axios.js
@@ -1,12 +1,35 @@
-
 import axios from "axios"
 
-const api = axios.create({
-    baseURL: import.meta.env.VITE_API_URL || "http:localhost:3000/api",
-    timeout: 60000,
-    withCredentials: false,
+// Using this to make different interceptors
+const tasksApi = axios.create({
+    baseURL: import.meta.env.VITE_API_URL || "http://localhost:3000/api",
+    sessionTimeout: 60000, // 60 seconds timeout
+    withCredentials: false, // credentials not been used, making it false
     headers: {
-        "Content-Type": "application/json",
+        "Content-type": "application/json",
         "Accept": "application/json"
     }
 })
+
+// Request interceptor 
+
+tasksApi.interceptors.request.use(
+    ( config ) => {
+        // Retry config if the request fails by any reason
+        config.retry = 3;
+        config.retryDelay = 1000
+
+        // Ensuring that the headers are properly set 
+
+        config.headers = {
+            // what this below line does is it stores the headers explicitly so that they aren't lost and stored into the system
+            ...config.headers,
+            "Content-Type" : "application/json",
+            "Accept": "application/json"
+        }
+        return config;
+    },
+    ( error ) => {
+        return Promise.reject(error)
+    }
+)
