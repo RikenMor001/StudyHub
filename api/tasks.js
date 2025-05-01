f185cd591058d01c6cb4e994ab7d9b2d1e00c674
Riken Patel <149461465+RikenMor001@users.noreply.github.com>
2025-05-01 18:48:30 -0400

    Add files via upload

---
+++ api/tasks.js
@@ -0,0 +1,20 @@
+// Get all the tasks
+// Create a new task 
+// Update the tasks
+// Delete the tasks
+
+import tasksApi from "./axios"
+
+export const useTaskApi = {
+    getTasks: async ( userId ) => {
+        try{
+        const response = await tasksApi.get(`/tasks/${userId}`);
+        if (response){
+            console.log("Tasks fetched")
+        }
+    } catch(error){
+        console.log("Error caught is " + error)
+        throw new Error();
+    }
+    }
+}
