b2e94c888537d179d1f22e580eeb87d48d348fd0
Riken Patel <149461465+RikenMor001@users.noreply.github.com>
2025-05-06 17:45:45 -0400

    Update LandingPage.jsx

--- pages/LandingPage.jsx
+++ pages/LandingPage.jsx
@@ -144,6 +144,22 @@ export default function LandingPage(){
                         </div>
                     </div>
                 </section>
+
+                {/*Community joining section*/}
+                <section className="py-20 bg-blue-50"> 
+                    <div className="container mx-auto max-w-6xl">
+                        <div className="text-center">
+                            <h2 className="text-3xl font-bold">
+                                Ready to transform your journey
+                            </h2>
+                            <p className="text-md font-semibold text-slate-500 mt-3">Join our community of successful students and start achieving your goals through talking to likewise minded people</p>
+                            <button className="bg-blue-500 text-white px-5 py-3 rounded-lg shadow-md mt-6 inline-flex items-center">
+                                Get Started Now
+                                <ArrowRight className="ml-2"/>
+                            </button>
+                        </div>
+                    </div>
+                </section>
             </main>
         </div>
     )
