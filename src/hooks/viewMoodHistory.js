c6879787af45748e524e08527ab3a72d1ecaca43
Riken Patel <149461465+RikenMor001@users.noreply.github.com>
2025-05-07 00:43:56 -0400

    Add files via upload

---
+++ src/hooks/viewMoodHistory.js
@@ -0,0 +1,8 @@
+
+import { useState } from "react"
+
+const viewMoodHistory = () => {
+    const [moodHistory, setMoodHistory] = useState([]);
+    const [loading, setLoading] = useState([]);
+    const [error, setError] = useState([]);
+}
