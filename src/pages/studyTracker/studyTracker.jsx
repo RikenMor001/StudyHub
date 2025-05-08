cf0b7cc381d93787792e962ecbd9bcb72757f862
Riken Patel <149461465+RikenMor001@users.noreply.github.com>
2025-05-08 01:05:14 -0400

    Update studyTracker.jsx

---
+++ src/pages/studyTracker/studyTracker.jsx
@@ -0,0 +1,19 @@
+// startTime, 
+// endTime,
+// duration, 
+// notes, 
+// subject, 
+// sessions, 
+// isStudying, 
+// breakTime 
+
+import { useState } from "react"
+
+const StudyTracker = () => {
+    const [startTime, setStartTime] = useState(null);
+    const [endTime, setEndTime] = useState(null);
+    const [duration, setDuration] = useState("");
+
+} 
+
+export { StudyTracker }; 
