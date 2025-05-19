9fc8d4a592187b8483bfd8d2294088163c37dde1
Riken Patel <149461465+RikenMor001@users.noreply.github.com>
2025-05-19 16:56:59 -0400

    Update habitBuilder.jsx

--- src/pages/habitBuilder/habitBuilder.jsx
+++ src/pages/habitBuilder/habitBuilder.jsx
@@ -2,13 +2,13 @@
 export const HabitBuilder = () => {
     return <div className="bg-slate-100 min-h-screen">
         <div className="sticky top-0 max-w-8xl flex justify-between">
-            <div className="flex justify-center items-center">
+            <div className="font-semibold text-lg text-slate-700">
                 Chat
             </div>
-            <div>
+            <div className="font-semibold text-lg text-slate-700">
                 Calender
             </div>
-            <div>
+            <div className="font-semibold text-lg text-slate-700">
                 Mood
             </div>
         </div>
