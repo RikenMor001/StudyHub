12439fc14ab5d1514e411f63393b69fba2b0acb3
Riken Patel <149461465+RikenMor001@users.noreply.github.com>
2025-05-19 16:37:49 -0400

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -5,9 +5,9 @@ import './App.css'
 import LandingPage from './pages/LandingPage'
 import { NavBar } from './components/NavBar'
 import { ThemeProvider } from './components/theme-provider'
-import { StudyTracker } from './pages/studyTracker/studyTracker'
 import { HabitBuilder } from './pages/habitBuilder/habitBuilder'
 import { Resources } from './pages/resources/resources'
+import { StudyTracker } from './pages/studyTracker/studyTracker'
 
 function App() {
   return <div>
@@ -18,7 +18,7 @@ function App() {
           <Routes>
             <Route path='/' element = {<LandingPage/>}/>
             <Route path='/study-tracker' element = {<StudyTracker/>}/>
-            <Route path='/habit-builder' element={<HabitBuilder/>}/>
+            <Route path='habit-builder' element = {<HabitBuilder/>}/>
             <Route path='/resources' element={<Resources/>}/>
           </Routes>
         </div>
