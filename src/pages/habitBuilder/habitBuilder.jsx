8d64bcd184d2e157a23134835726f05c13dfdad3
Riken Patel <149461465+RikenMor001@users.noreply.github.com>
2025-05-19 16:54:24 -0400

    Update habitBuilder.jsx

--- src/pages/habitBuilder/habitBuilder.jsx
+++ src/pages/habitBuilder/habitBuilder.jsx
@@ -1,8 +1,16 @@
 
 export const HabitBuilder = () => {
     return <div className="bg-slate-100 min-h-screen">
-        <div>
-            
+        <div className="sticky top-0 max-w-8xl flex justify-between">
+            <div className="flex justify-center items-center">
+                Chat
+            </div>
+            <div>
+                Calender
+            </div>
+            <div>
+                Mood
+            </div>
         </div>
     </div>
 }
