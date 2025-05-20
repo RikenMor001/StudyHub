ffa537805df04c4cbe55419651308da2f193d35c
Riken Patel <149461465+RikenMor001@users.noreply.github.com>
2025-05-20 17:00:10 -0400

    Update tabs.jsx

--- src/ui/tabs.jsx
+++ src/ui/tabs.jsx
@@ -1,45 +1,17 @@
 
 import * as React from "react"
-import TabsPrimitive from "@radix-ui/react-tabs"
-import { cn } from "../lib/utils";
+import * as TabsPrimitive from "@radix-ui/react-tabs"
+import cn from "../lib/utils"
 
-export const Tabs = TabsPrimitive.Root
+export const Tabs = TabsPrimitive.Root;
 
-export const TabsList = React.forwardRef(({className, ...props}, ref)=> (
+export const TabsList = React.forwardRef(({className, ...props}, ref) => (
     <TabsPrimitive.List
-        ref = {ref}
-        className = {cn(
-            "",
-            className
-        )}
-        {...props}
-    />
-))
-
-TabsList.displayName = TabsPrimitive.List.displayName;
-
-export const TableTabs = React.forwardRef(({className, ...props}, ref) => (
-    <TablePrimitive.Triger
     ref = {ref}
-    className = {cn(
+    className={cn(
         "",
         className
     )}
     {...props}
     />
 ))
-
-TableTabs.displayName = TabsPrimitive.Trigger.displayName;
-
-export const TabsContent = React.forwardRef(({className, ...props}, ref) => (
-    <TabsPrimitive.Content
-        ref = {ref}
-        className = {cn(
-            "",
-            className
-        )}
-        {...props}
-    />
-))
-
-TabsContent.displayName = TabsContent.Content.displayName;
