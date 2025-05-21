deca1d1f85ddb64ed752cacaa5279414bbd54f5f
Riken Patel <149461465+RikenMor001@users.noreply.github.com>
2025-05-21 00:34:28 -0400

    Update habitBuilder.jsx

--- src/pages/habitBuilder/habitBuilder.jsx
+++ src/pages/habitBuilder/habitBuilder.jsx
@@ -1,16 +1,21 @@
+// import { useState } from "react"
+// import { Tabs, TabsList, TabsTrigger } from "../../ui/tabs"
 
 export const HabitBuilder = () => {
-    return <div className="bg-slate-100 min-h-screen">
-        <div className="sticky top-0 max-w-8xl flex justify-between">
-            <div className="font-semibold text-lg text-slate-700">
-                Chat
-            </div>
-            <div className="font-semibold text-lg text-slate-700">
-                Calender
-            </div>
-            <div className="font-semibold text-lg text-slate-700">
-                Mood
+    
+        return <div className="flex flex-col bg-slate-100 h-screen">
+            <div className="container mx-auto px-2 sm:px-4 overflow-hidden">
+                <div className="bg-white rounded-lg shadow-md p-4 inline-flex gap-165">
+                    <div className="text-lg font-semibold tracking-tight text-slate-600">
+                        Chat
+                    </div>
+                    <div className="text-lg font-semibold tracking-tight text-slate-600">
+                        Calender
+                    </div>
+                    <div className="text-lg font-semibold tracking-tight text-slate-600">
+                        Mood
+                    </div>
+                </div>
             </div>
         </div>
-    </div>
 }
