077a4b0bbfdd9cec2e795f9887c0459bf5eeb0f2
Riken Patel <149461465+RikenMor001@users.noreply.github.com>
2025-05-22 19:17:16 -0400

    Update tabs.jsx

--- src/ui/tabs.jsx
+++ src/ui/tabs.jsx
@@ -9,7 +9,7 @@ export const TabsList = React.forwardRef(({className, ...props}, ref) => (
     <TabsPrimitive.List
     ref = {ref}
     className={cn(
-        "inline-flex items-center justify-center rounded-md bg-white p-1 text-slate-700 shadoow-lg", 
+        "inline-flex items-center justify-center rounded-md bg-white p-1 text-black shadoow-lg border border-slate-200 shadow-sm hover:cursor-pointer", 
         className
     )}
     {...props}
@@ -22,7 +22,7 @@ export const TabsTrigger = React.forwardRef(({className, ...props}, ref) => (
     <TabsPrimitive.List
     ref = {ref}
     className={cn(
-        "inline-flex items-center jusitfy-center roundedn-md px-3 py-2 text-sm font-medium text-slate-700 whitespace-nowrap",
+        "inline-flex items-center justify-center whitespace-nowrap rounded-sm px-3 py-2",
         className
     )}
     {...props}
@@ -35,7 +35,7 @@ export const TabsContent = React.forwardRef(({className, ...props}, ref) => (
     <TabsPrimitive.List
     ref = {ref}
     className={cn(
-        "",
+        "inline-flex items-center justify-center whitespace-nowrap rounded-sm px-3 py-2",
         className
     )}  
     {...props}
