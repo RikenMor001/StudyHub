189545808fc6fc191c7a4df7a9f4540f7b9d1c78
Riken Patel <149461465+RikenMor001@users.noreply.github.com>
2025-05-22 18:05:52 -0400

    Update habitBuilder.jsx

--- src/pages/habitBuilder/habitBuilder.jsx
+++ src/pages/habitBuilder/habitBuilder.jsx
@@ -1,21 +1,27 @@
 // import { useState } from "react"
 // import { Tabs, TabsList, TabsTrigger } from "../../ui/tabs"
 
+import { useState } from "react"
+import { Tabs, TabsList, TabsTrigger } from "../../ui/tabs"
+
 export const HabitBuilder = () => {
-    
+        const [currentView, setCurrentView] = useState("chat");
+
         return <div className="flex flex-col bg-slate-100 h-screen">
-            <div className="container mx-auto px-2 sm:px-4 overflow-hidden">
-                <div className="bg-white rounded-lg shadow-md p-4 inline-flex gap-165">
-                    <div className="text-lg font-semibold tracking-tight text-slate-600">
-                        Chat
-                    </div>
-                    <div className="text-lg font-semibold tracking-tight text-slate-600">
-                        Calender
-                    </div>
-                    <div className="text-lg font-semibold tracking-tight text-slate-600">
-                        Mood
-                    </div>
-                </div>
-            </div>
+            <main className="flex-1 container mx-auto px-2 sm:px-4 overflow-hidden">                          
+                <Tabs className = "h-full flex flex-col" value = {currentView} onValueChange = {setCurrentView}>
+                    <TabsList>
+                        <TabsTrigger>
+                            Chat
+                        </TabsTrigger>
+                        <TabsTrigger>
+                            Calender
+                        </TabsTrigger>
+                        <TabsTrigger>
+                            Mood
+                        </TabsTrigger>
+                    </TabsList>
+                </Tabs>
+            </main>
         </div>
 }
