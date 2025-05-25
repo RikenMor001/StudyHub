342ac27d5f1ddf3929df55389db4e4f153c22c11
Riken Patel <149461465+RikenMor001@users.noreply.github.com>
2025-05-24 22:17:03 -0400

    Update habitBuilder.jsx

--- src/pages/habitBuilder/habitBuilder.jsx
+++ src/pages/habitBuilder/habitBuilder.jsx
@@ -27,7 +27,7 @@ export const HabitBuilder = () => {
                     </TabsList>
 
                     <div className="">
-                        <button className="border border-slate-300 rounded-md px-3 py-2 shadow-md bg-white text-black" 
+                        <button className="border border-slate-300 rounded-md px-3 py-2 shadow-md bg-white text-black hover:cursor-pointer hover:bg-slate-100" 
                         onClick={() => setUseGemini(!useGemini)}
                         >
                             {useGemini ? "Using: Default AI" : "Using: Gemini AI"}
