db99671d997574ee351743e9e045a03b33b515e2
Riken Patel <149461465+RikenMor001@users.noreply.github.com>
2025-06-02 15:55:43 -0400

    Update resources.jsx

--- src/pages/resources/resources.jsx
+++ src/pages/resources/resources.jsx
@@ -3,17 +3,17 @@ export const Resources = () => {
     return (
         <div className="bg-slate-100 h-screen container mx-auto py-8 px-4">
             <div className="max-w-4xl mx-auto">
-                <div className="text-3xl font-semibold flex justify-center items-center pt-7">
+                <div className="text-3xl font-semibold flex justify-center items-center p-5">
                     Learning Resources
                 </div>
-                <div className="inline-flex justify-center items-center gap-60 pt-4">
-                    <div>
+                <div className="inline-flex justify-center items-center gap-50 border border-slate-200 rounded-lg "> 
+                    <div className="text-xl font-semibold traking-tight hover:cursor-pointer hover:text-slate-700">
                         Educational News 
                     </div>
-                    <div>
+                    <div className="text-xl font-semibold traking-tight hover:cursor-pointer hover:text-slate-700">
                         Recommended Books 
                     </div>
-                    <div>
+                    <div className="text-xl font-semibold traking-tight hover:cursor-pointer hover:text-slate-700">
                         Learning Tips 
                     </div>
                 </div>
