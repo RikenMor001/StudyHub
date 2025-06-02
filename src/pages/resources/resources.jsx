90a7ac41dfe94b37ec3b0fd5d521fdd63f8ee32d
Riken Patel <149461465+RikenMor001@users.noreply.github.com>
2025-06-02 15:36:37 -0400

    Update resources.jsx

--- src/pages/resources/resources.jsx
+++ src/pages/resources/resources.jsx
@@ -1,9 +1,23 @@
 
-import * as React from "react"
 export const Resources = () => {
     return (
-        <div>
-            Resources page coming soon
+        <div className="bg-slate-100 h-screen container mx-auto py-8 px-4">
+            <div className="max-w-4xl mx-auto">
+                <div className="text-3xl font-semibold flex justify-center items-center pt-7">
+                    Learning Resources
+                </div>
+                <div className="inline-flex justify-center items-center gap-60 pt-4">
+                    <div>
+                        Educational News 
+                    </div>
+                    <div>
+                        Recommended Books 
+                    </div>
+                    <div>
+                        Learning Tips 
+                    </div>
+                </div>
+            </div>
         </div>
     )
-}
+}  
